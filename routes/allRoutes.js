051169f255fb933e6a34c5253cca460db2d8fb63
İsmail ÖZCAN <io08@outlook.com>
2016-08-07 22:23:02 +0300

    a

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -2,5 +2,5 @@
     { route : '/admin/' , method : require('./admin.js') },
     { route : '/api/' , method : require('./register.js') },
     { route : '/api/' , method : require('./login.js') } ,
-    { route : '/api/auth' , method : require('./auth/authIndex.js') } 
+    { route : '/api/auth/' , method : require('./auth/authIndex.js') } 
 ];
